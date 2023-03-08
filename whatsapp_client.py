e809967de52af8431a86da789c19e3e1f4552820
Patricio Urrutia <patricio.urrutia.n@gmail.com>
2023-03-08 10:46:19 -0300

    update

--- whatsapp_client.py
+++ whatsapp_client.py
@@ -311,7 +311,7 @@ class WhatsAppWrapper:
     
     async def process_webhook_notification(self, data):
         response = []
-        changes =   data['entry'][0]['changes'][0]['value']
+        changes =  await data['entry'][0]['changes'][0]['value']
         print(changes)
         # connection = pymysql.connect(host='10.10.1.216',
         # user='root',
