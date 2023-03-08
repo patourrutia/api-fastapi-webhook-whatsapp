bdb6ce8f0571bdb9b760ad88e200f14819afda7f
Patricio Urrutia <patricio.urrutia.n@gmail.com>
2023-03-08 10:18:52 -0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI, Request, Response
-import asyncio
+#import asyncio
 
 
 
@@ -35,5 +35,5 @@ async def verify(request: Request):
     data_json = await request.json()
     #print(data_json)
     client = WhatsAppWrapper()
-    response = asyncio.run(client.process_webhook_notification(data_json))
+    response =client.process_webhook_notification(data_json)
     
