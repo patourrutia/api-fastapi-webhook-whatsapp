010a251b05d261125830b008a177776a5a25937c
Patricio Urrutia <patricio.urrutia.n@gmail.com>
2023-03-28 20:28:07 -0300

    update

--- whatsapp_client.py
+++ whatsapp_client.py
@@ -19,7 +19,7 @@ def call_gpt2(preg):
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "user", "content": preg}
+        {"role": "system", "content": preg}
      ]
     )
     # print (completion.choices[0].message["content"])
